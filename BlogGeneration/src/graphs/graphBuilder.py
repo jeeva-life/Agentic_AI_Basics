f3595e952e121881af3c799117455444b1a09de7
jeeva-life <kodalijeevan756@gmail.com>
2025-07-19 17:16:48 -0400

    executed fastapi using uvicorn

--- BlogGeneration/src/graphs/graphBuilder.py
+++ BlogGeneration/src/graphs/graphBuilder.py
@@ -14,12 +14,19 @@ class GraphBuilder:
         """
         blog_node_obj=BlogNode(self.llm)
         #Nodes
-        self.graph.add_node("title_creation",self.blog_node_obj.title_creation)
-        self.graph.add_node("content_generator",self.blog_node_obj.content_generator)
+        self.graph.add_node("title_creation",blog_node_obj.title_creation)
+        self.graph.add_node("content_generator",blog_node_obj.content_generator)
 
         #   Edges
         self.graph.add_edge(START,"title_creation")
-        self.graph.add_edge("title_creation","content_generation")
-        self.graph.add_edge("content_generation",END)
+        self.graph.add_edge("title_creation","content_generator")
+        self.graph.add_edge("content_generator",END)
 
         return self.graph
+    
+
+    def setup_graph(self, usecase):
+        if usecase == "topic":
+            self.build_topic_graph()
+
+        return self.graph.compile()
