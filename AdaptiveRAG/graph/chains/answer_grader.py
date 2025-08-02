0f7eeed1fc1209adf666d00a9ae42768287e0a69
jeeva-life <kodalijeevan756@gmail.com>
2025-08-02 15:35:22 -0500

    added logic for answer grader checks whether it answers to the users question

---
+++ AdaptiveRAG/graph/chains/answer_grader.py
@@ -0,0 +1,27 @@
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_core.runnables import RunnableSequence
+from pydantic import BaseModel, Field
+from model import llm_model
+
+llm = llm_model
+
+class AnswerGrade(BaseModel):
+    """Binary score for answer quality"""
+    binary_score: bool = Field(
+        description="Answer is correct, 'yes' or 'no'"
+    )
+
+structured_llm_grader = llm.with_structured_output(AnswerGrade)
+
+system = """
+You are a grader assessing whether an answer addresses / resolves a question \n 
+     Give a binary score 'yes' or 'no'. Yes' means that the answer resolves the question."""
+
+answer_prompt = ChatPromptTemplate.from_messages(
+    [
+        ("system", system),
+        ("human","User question: \n\n {question} \n\n LLM generation: {generation}"),
+    ]
+)
+
+answer_grader: RunnableSequence = answer_prompt | structured_llm_grader
